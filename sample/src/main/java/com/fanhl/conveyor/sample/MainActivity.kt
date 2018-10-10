5fce94dc3af39dcaa17d2eddd907948864efef48
fanhl <dyguests@qq.com>
2018-10-10 22:31:49 +0800

    it works

--- sample/src/main/java/com/fanhl/conveyor/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/conveyor/sample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.fanhl.conveyor.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.fanhl.conveyor.Poper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val t: Poper
     }
 }
